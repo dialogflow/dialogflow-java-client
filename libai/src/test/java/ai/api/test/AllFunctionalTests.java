c4e5610b9c848ec9e584b5508cca1d14c3cc5c8b
folomeev@speaktoit.com <folomeev@speaktoit.com>
2016-11-11 12:51:47 +0600

    Got rid of VoiceActivityDetection

--- libai/src/test/java/ai/api/test/AllFunctionalTests.java
+++ libai/src/test/java/ai/api/test/AllFunctionalTests.java
@@ -6,13 +6,11 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import ai.api.test.compatibility.DefaultProtocolTest;
 import ai.api.test.compatibility.V20150204ProtocolTest;
-import ai.api.util.VADTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	/* ProtocolDevTest.class, */
 	ProtocolProdTest.class,
-	VADTest.class,
 	DefaultProtocolTest.class,
 	V20150204ProtocolTest.class
 	})
