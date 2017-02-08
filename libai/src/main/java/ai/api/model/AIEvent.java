bb44e7789884e4f2cd89b2533cb4e3058ab3315d
folomeev <folomeev@speaktoit.com>
2017-02-08 17:06:19 +0700

    Compiler warning fixed

--- libai/src/main/java/ai/api/model/AIEvent.java
+++ libai/src/main/java/ai/api/model/AIEvent.java
@@ -6,67 +6,66 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by alexey on 07/12/2016.
- */
 public class AIEvent implements Serializable {
-    @SerializedName("name")
-    private String name;
+  private static final long serialVersionUID = 1L;
 
-    @SerializedName("data")
-    private Map<String, String> data;
+  @SerializedName("name")
+  private String name;
 
-    public AIEvent(){
+  @SerializedName("data")
+  private Map<String, String> data;
 
+  public AIEvent() {
+
+  }
+
+  public AIEvent(final String name) {
+    this.name = name;
+  }
+
+  /**
+   * Event name
+   */
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * String data map
+   */
+  public Map<String, String> getData() {
+    return data;
+  }
+
+  public void setData(Map<String, String> data) {
+    this.data = data;
+  }
+
+  public void addDataField(String key, String value) {
+    if (data == null)
+      setData(new HashMap<String, String>());
+    data.put(key, value);
+  }
+
+  public void addDataField(Map<String, String> dataParams) {
+    if (data == null)
+      setData(new HashMap<String, String>());
+    data.putAll(dataParams);
+  }
+
+  public String getDataField(final String name) {
+    return getDataField(name, "");
+  }
+
+  public String getDataField(final String name, final String defaultValue) {
+    if (data.containsKey(name)) {
+      return data.get(name);
     }
-
-    public AIEvent(final String name) {
-        this.name = name;
-    }
-
-    /**
-     * Event name
-     */
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * String data map
-     */
-    public Map<String, String> getData() {
-        return data;
-    }
-
-    public void setData(Map<String, String> data) {
-        this.data = data;
-    }
-
-    public void addDataField(String key, String value) {
-        if (data == null)
-            setData(new HashMap<String, String>());
-        data.put(key, value);
-    }
-
-    public void addDataField(Map<String, String> dataParams) {
-        if (data == null)
-            setData(new HashMap<String, String>());
-        data.putAll(dataParams);
-    }
-
-    public String getDataField(final String name) {
-        return getDataField(name, "");
-    }
-
-    public String getDataField(final String name, final String defaultValue) {
-        if (data.containsKey(name)) {
-            return data.get(name);
-        }
-        return defaultValue;
-    }
+    return defaultValue;
+  }
 
 }
