8851dfa914450aaa08e22d2f49c3d65cd76ae058
Matt Carroll <matthewcarroll@matthewcarroll-macbookpro2.roam.corp.google.com>
2017-03-28 16:38:20 -0700

    Move samples

---
+++ samples/lego/README.md
@@ -0,0 +1,18 @@
+# Android voice control app for Lego® Mindstorms EV3.
+The voice interaction scenarios were built with [Api.ai](api.ai).
+
+## Getting started
+1. Register with [Api.ai](http://api.ai)
+2. Create an agent
+3. Import agent from the zip file **agent/apiai_lego_agent.zip**. Import can be done from the agent's settings page.
+4. Use agent's API keys in the app an build the APK
+
+## Using the app
+0. Pre-requisite: EV3 needs to be running LejOS - see instructions [here](http://sourceforge.net/p/lejos/wiki/Installing%20leJOS/)
+1. Start the app
+2. Open application settings (top right corner)
+3. Specify keys from the agent and EV3's IP address
+4. Say a voice command, e.g. "Move 1 step forward"
+
+## Based on the libs:
+- LejOS - http://www.lejos.org/ev3.php
---
+++ samples/twilio/src/main/java/ai/api/twilio/TwilioProperties.java
@@ -0,0 +1,80 @@
+package ai.api.twilio;
+
+/***********************************************************************************************************************
+ *
+ * API.AI Twilio Helper Libs
+ * =================================================
+ *
+ * Copyright (C) 2015 by Speaktoit, Inc / Api.ai (https://speaktoit.com)
+ * https://api.ai
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************/
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Twilio properties
+ */
+public final class TwilioProperties {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TwilioProperties.class);
+	private static final String TWILIO_PROP_NAME = "twilio.properties";
+
+	public static final TwilioProperties INSTANCE = new TwilioProperties();
+
+	private final Properties properties;
+
+	private TwilioProperties() {
+		properties = new Properties();
+		try {
+			properties.load(this.getClass().getClassLoader().getResourceAsStream(TWILIO_PROP_NAME));
+		} catch (IOException e) {
+			LOGGER.error("Failed to parse properties: " + TWILIO_PROP_NAME, e);
+		}
+	}
+
+	public String get(String name) {
+		return properties.getProperty(name);
+	}
+
+	/**
+	 * Url to api.ai server
+	 * 
+	 * @return
+	 */
+	public String getApiUrl() {
+		return get("apiai.url");
+	}
+
+	/**
+	 * api.ai user access token
+	 * 
+	 * @return
+	 */
+	public String getApiAccessToken() {
+		return get("apiai.accessToken");
+	}
+
+	/**
+	 * Timezone for api.ai requests
+	 * 
+	 * @return
+	 */
+	public String getTimezone() {
+		return properties.getProperty("apiai.timezone", "-0700");
+	}
+}
